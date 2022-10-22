f87858a0102403730e6eee95c50b0417e48f9da8
Vanja Sretenović <31417903+vanja032@users.noreply.github.com>
2022-10-22 20:52:18 +0200

    Create ServerCryptographyFunctions.php

---
+++ ServerCryptographyFunctions.php
@@ -0,0 +1,14 @@
+<?php
+$method = "aes-256-cbc";
+$iv = "hdj27al8id5vmd45";
+$key = substr(hash("sha256", $connection_hash_key, true), 0, 32);
+$functionEncryptData = function($data) use($method, $iv, $key){
+    return base64_encode(openssl_encrypt($data, $method, $key, OPENSSL_RAW_DATA, $iv));
+};
+$functionDecryptData = function($data) use($method, $iv, $key){
+    return openssl_decrypt(base64_decode($data), $method, $key, OPENSSL_RAW_DATA, $iv);
+};
+$functionPassword = function(){
+    return strval(explode(" ", microtime())[1].explode(" ", microtime())[0]);
+};
+?>
