9545bf59b2a5277b76c8ae7a59555f2a648d44e7
Vanja Sretenović <31417903+vanja032@users.noreply.github.com>
2022-10-22 20:52:48 +0200

    Update ServerCryptographyFunctions.php

--- ServerCryptographyFunctions.php
+++ ServerCryptographyFunctions.php
@@ -1,6 +1,8 @@
 <?php
 $method = "aes-256-cbc";
 $iv = "hdj27al8id5vmd45";
+$connection_hash_key = "dsjhjhdhdusihdish";
+
 $key = substr(hash("sha256", $connection_hash_key, true), 0, 32);
 $functionEncryptData = function($data) use($method, $iv, $key){
     return base64_encode(openssl_encrypt($data, $method, $key, OPENSSL_RAW_DATA, $iv));
